0afaeb162838689d0632a9039a2183be8dd1cff5
wendellmva <wmuntslag@live.nl>
2020-07-13 12:50:41 +0200

    chore: add mmenu-light demo

--- src/app/mmenu.directive.ts
+++ src/app/mmenu.directive.ts
@@ -1,12 +1,44 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, OnInit } from '@angular/core';
+import * as MmenuLight from 'mmenu-light';
 
 @Directive({
   selector: '[appMmenu]'
 })
-export class MmenuDirective {
+export class MmenuDirective implements OnInit {
 
-  constructor() {
-    console.log('init');
+  constructor(private el: ElementRef){
+    console.log('ctor');
+    console.log(MmenuLight)
    }
+
+  ngOnInit(){
+    console.log('init');
+    // MmenuLight(
+    //   this.el.nativeElement, "(max-width: 600px)"
+    // );
+
+    const menu = new MmenuLight(
+				this.el.nativeElement,
+				'all'
+			);
+
+    const navigator = menu.navigation({
+      // selectedClass: 'Selected',
+      // slidingSubmenus: true,
+      // theme: 'dark',
+      // title: 'Menu'
+    });
+
+    const drawer = menu.offcanvas({
+      // position: 'left'
+    });
+
+    //	Open the menu.
+    document.querySelector( 'a[href="#menu"]' )
+      .addEventListener( 'click', evnt => {
+        evnt.preventDefault();
+        drawer.open();
+      });
+  }
 
 }
